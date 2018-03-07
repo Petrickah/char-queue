f28a26adac9864c9e6942b051bf6243d2228e634
Tiberiu Petre <tiberiu@localhost.localdomain>
2018-03-07 18:46:53 +0200

    Implementat constructori si destructori in clase

--- tema1/src/charQueue.cpp
+++ tema1/src/charQueue.cpp
@@ -1,1 +1,24 @@
 #include "headers/charQueue.hpp"
+
+Node::Node(char myChar) { this->myChar = myChar; nextNode = nullptr; }
+Node::Node() { this->myChar = '\0'; nextNode = nullptr; }
+Node::~Node() { 
+    while(nextNode != nullptr) {
+        delete this->nextNode;
+        this->nextNode = this->nextNode->nextNode;
+    }
+}
+Node* Node::getNextNode() { return this->nextNode; }
+void Node::setNextNode(Node* myNextNode) { this->nextNode = myNextNode; }
+char Node::getChar() { return this->myChar; }
+void Node::setChar(char myNewChar) { this->myChar = myNewChar; }
+
+CharQueue::CharQueue() { this->firstNode = nullptr; this->lastNode = nullptr; }
+CharQueue::~CharQueue() {
+    while(firstNode != lastNode) {
+        Node *p = firstNode->getNextNode();
+        delete firstNode;
+        firstNode = p;
+    }
+    delete lastNode;
+}
--- tema1/src/headers/charQueue.hpp
+++ tema1/src/headers/charQueue.hpp
@@ -1,4 +1,22 @@
 #ifndef POO_TEMA1_CHARQUEUE_H_
 #define POO_TEMA1_CHARQUEUE_H_
 
+class Node {
+    char myChar; Node* nextNode;
+public:
+    Node(char myChar); Node(); ~Node(); //Constructori + Destructori
+    //Metode
+    Node* getNextNode();
+    void setNextNode(Node* myNextNode);
+    char getChar();
+    void setChar(char myNewChar);
+};
+
+class CharQueue {
+    Node* firstNode, *lastNode;
+public:
+    CharQueue(); ~CharQueue(); //Constructor + Destructor
+    //Metode
+};
+
 #endif // POO_TEMA1_CHARQUEUE_H_
