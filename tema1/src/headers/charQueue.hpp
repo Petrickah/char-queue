77fa0e14943dc4a97744d64724a4f9b08f408eea
Tiberiu Petre <tiberiu@localhost.localdomain>
2018-03-07 19:13:32 +0200

    Implementare citire si afisare coada

--- tema1/src/headers/charQueue.hpp
+++ tema1/src/headers/charQueue.hpp
@@ -1,22 +1,35 @@
 #ifndef POO_TEMA1_CHARQUEUE_H_
 #define POO_TEMA1_CHARQUEUE_H_
 
-class Node {
-    char myChar; Node* nextNode;
-public:
-    Node(char myChar); Node(); ~Node(); //Constructori + Destructori
-    //Metode
-    Node* getNextNode();
-    void setNextNode(Node* myNextNode);
-    char getChar();
-    void setChar(char myNewChar);
-};
+#include <iostream>
 
-class CharQueue {
-    Node* firstNode, *lastNode;
-public:
-    CharQueue(); ~CharQueue(); //Constructor + Destructor
-    //Metode
-};
+namespace NodeClass {
+    class Node {
+        char myChar; Node* nextNode;
+    public:
+        Node(char myChar); Node(); ~Node(); //Constructori + Destructori
+        //Metode
+        Node* getNextNode();
+        void setNextNode(Node* myNextNode);
+        char getChar();
+        void setChar(char myNewChar);
+    };
+}
+
+namespace CharQueueClass {
+    class CharQueue {
+        NodeClass::Node* firstNode, *lastNode;
+    public:
+        CharQueue(); ~CharQueue(); //Constructor + Destructor
+        //Metode
+        void push(char myChar);
+        char pop();
+        bool isEmpty();
+        friend std::ostream& operator<<(std::ostream& out, CharQueue myQueue);
+        friend std::istream& operator>>(std::istream& in, CharQueue& myQueue);
+        CharQueue& operator+(CharQueue myQueue);
+        CharQueue& operator-(CharQueue myQueue);
+    };
+}
 
 #endif // POO_TEMA1_CHARQUEUE_H_
--- tema1/src/main.cpp
+++ tema1/src/main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "headers/charQueue.hpp"
 
 int main(int argc, char* argv[]) {
