a4192f7c8790c30b5f6a6d2ef22a189e537d5d24
Tiberiu Petre <tiberiu@localhost.localdomain>
2018-03-12 16:35:25 +0200

    resetare clase

--- tema1/src/charQueue.cpp
+++ tema1/src/charQueue.cpp
@@ -1,63 +1,7 @@
 #include "headers/charQueue.hpp"
 
 namespace NodeClass {
-    Node::Node(char myChar) { this->myChar = myChar; nextNode = nullptr; }
-    Node::Node() { this->myChar = '\0'; nextNode = nullptr; }
-    Node::~Node() { 
-        while(nextNode != nullptr) {
-            delete this->nextNode;
-            this->nextNode = this->nextNode->nextNode;
-        }
-    }
-    Node* Node::getNextNode() { return this->nextNode; }
-    void Node::setNextNode(Node* myNextNode) { this->nextNode = myNextNode; }
-    char Node::getChar() { return this->myChar; }
-    void Node::setChar(char myNewChar) { this->myChar = myNewChar; }
 }
 
 namespace CharQueueClass {
-    CharQueue::CharQueue() { this->firstNode = nullptr; this->lastNode = nullptr; }
-    CharQueue::~CharQueue() {
-        while(firstNode != lastNode) {
-            NodeClass::Node *p = firstNode->getNextNode();
-            delete firstNode;
-            firstNode = p;
-        }
-        delete lastNode;
-    }
-    bool CharQueue::isEmpty() {
-        return (this->firstNode == this->lastNode && this->firstNode == nullptr);
-    }
-    void CharQueue::push(char myChar) {
-        if(isEmpty())
-            firstNode = lastNode = new NodeClass::Node(myChar);
-        else {
-            lastNode->setNextNode(new NodeClass::Node(myChar));
-            this->lastNode = lastNode->getNextNode();
-        }
-    }
-    char CharQueue::pop() {
-        char myChar = '\0';
-        if(firstNode != nullptr) {
-            NodeClass::Node* p = firstNode->getNextNode();
-            myChar = firstNode->getChar();
-            delete firstNode;
-            firstNode = p;
-        }
-        return myChar;
-    }
-
-    std::ostream& operator<<(std::ostream& out, CharQueue myQueue) {
-        while(!myQueue.isEmpty()){
-            out<<myQueue.pop();
-        }
-    }
-    std::istream& operator>>(std::istream& in, CharQueue& myQueue) {
-        char str[250];
-        in.getline(str, 250);
-        for(char myChar: str) {
-            myQueue.push(myChar);
-        }
-        return in;
-    }
 }
--- tema1/src/headers/charQueue.hpp
+++ tema1/src/headers/charQueue.hpp
@@ -5,30 +5,13 @@
 
 namespace NodeClass {
     class Node {
-        char myChar; Node* nextNode;
-    public:
-        Node(char myChar); Node(); ~Node(); //Constructori + Destructori
-        //Metode
-        Node* getNextNode();
-        void setNextNode(Node* myNextNode);
-        char getChar();
-        void setChar(char myNewChar);
+     
     };
 }
 
 namespace CharQueueClass {
     class CharQueue {
-        NodeClass::Node* firstNode, *lastNode;
-    public:
-        CharQueue(); ~CharQueue(); //Constructor + Destructor
-        //Metode
-        void push(char myChar);
-        char pop();
-        bool isEmpty();
-        friend std::ostream& operator<<(std::ostream& out, CharQueue myQueue);
-        friend std::istream& operator>>(std::istream& in, CharQueue& myQueue);
-        CharQueue& operator+(CharQueue myQueue);
-        CharQueue& operator-(CharQueue myQueue);
+    
     };
 }
 
