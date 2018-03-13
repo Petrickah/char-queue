d875fafafdfd4ed90d000aecc27bf13291d04128
Tiberiu Petre <tiberiu@localhost.localdomain>
2018-03-13 21:36:47 +0200

    Release

--- tema1/src/main.cpp
+++ tema1/src/main.cpp
@@ -1,14 +1,28 @@
 #include "headers/charQueue.hpp"
 
 int main(int argc, char* argv[]) {
-    Classes::CharQueueClass::CharQueue myFirstQueue, mySecondQueue;
+    Classes::CharQueueClass::CharQueue myFirstQueue, mySecondQueue, myThirdQueue;
+    Classes::CharQueueClass::CharQueue myFirstResult, mySecondResult;
     /*myQueue.push('E'); myQueue.push('x'); myQueue.push('a');
     myQueue.push('m'); myQueue.push('e'); myQueue.push('n');*/
     std::cout<<"Introduceti elementele primei cozi: "; std::cin>>myFirstQueue;
-    std::cout<<"Introduceti elementele celei de a doua coada: "; std::cin>>mySecondQueue;
+    std::cout<<"Introduceti elementele celei de a doua cozi: "; std::cin>>mySecondQueue;
+    std::cout<<"Introduceti elementele celei de a treia cozi: "; std::cin>>myThirdQueue;
     //std::cout<<(myQueue.isEmpty()?"Coada este goala":"Coada contine cel putin un element")<<std::endl;
-    myFirstQueue = myFirstQueue + mySecondQueue;
-    std::cout<<"Noua coada dupa concatenare este: "<<myFirstQueue<<std::endl;
+    
+    std::cout<<"--------------------------------------------------------"<<std::endl;
+
+    myFirstResult = mySecondQueue + myThirdQueue;
+    std::cout<<"Noua coada dupa concatenare este: "<<myFirstResult<<std::endl;
+    mySecondResult = myFirstQueue - (mySecondQueue + myThirdQueue);
+    std::cout<<"Noua coada dupa diferentiere este: "<<mySecondResult<<std::endl;
+
+    std::cout<<"--------------------------------------------------------"<<std::endl;
+
+    std::cout<<"Prima coada este: "<<myFirstQueue<<std::endl;
+    std::cout<<"A doua coada este: "<<mySecondQueue<<std::endl;
+    std::cout<<"A treia coada este: "<<myThirdQueue<<std::endl;
+    
     //std::cout<<(myQueue.isEmpty()?"Coada este goala":"Coada contine cel putin un element")<<std::endl;
     return 0;
 }
