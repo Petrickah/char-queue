e33f7526427d14126b30ce1a3c80dcb6d5e95c4d
Tiberiu Petre <tiberiu@localhost.localdomain>
2018-03-13 21:24:18 +0200

    Finalizare proiect

--- tema1/src/headers/charQueue.hpp
+++ tema1/src/headers/charQueue.hpp
@@ -3,33 +3,36 @@
 
 #include <iostream>
 
-namespace NodeClass {
-    class Node {
-        char myChar; Node* nextNode;
-    public:
-        Node(char myChar); Node(); ~Node(); //Constructori + Destructori
-        //Metode
-        Node* getNextNode();
-        void setNextNode(Node* myNextNode);
-        char getChar();
-        void setChar(char myNewChar);
-    };
-}
+namespace Classes {
+    namespace NodeClass {
+        class Node {
+            char info; Node* nextNode;
+        public:
+            Node(); Node(char myInfo); Node(char myInfo, Node* nextNode); ~Node();
+            Node* getNextNode() { return nextNode; }
+            void setNextNode(Node* newNextNode) { nextNode = newNextNode; }
+            char getInfo() { return info; }
+            void setInfo(char myChar) { info = myChar; }
+        };
+    }
 
-namespace CharQueueClass {
-    class CharQueue {
-        NodeClass::Node* firstNode, *lastNode;
-    public:
-        CharQueue(); ~CharQueue(); //Constructor + Destructor
-        //Metode
-        void push(char myChar);
-        char pop();
-        bool isEmpty();
-        friend std::ostream& operator<<(std::ostream& out, CharQueue myQueue);
-        friend std::istream& operator>>(std::istream& in, CharQueue& myQueue);
-        CharQueue& operator+(CharQueue myQueue);
-        CharQueue& operator-(CharQueue myQueue);
-    };
+    namespace CharQueueClass {
+        class CharQueue {
+            NodeClass::Node* firstNode;
+            NodeClass::Node* lastNode;
+            int size;
+        public:
+            CharQueue(CharQueue& obj); //Constructor de copiere
+            CharQueue(); ~CharQueue(); //Constructor de initializare + destructor 
+            bool isEmpty(); //Metoda de verificare daca coada este vida
+            void push(char myChar); //Metoda de insertie a unui element nou in coada
+            char pop(); //Metoda de extragere si stergere a unui element din coada
+            friend std::ostream& operator<<(std::ostream&, CharQueue&); //Functie de citire a unei cozi
+            friend std::istream& operator>>(std::istream&, CharQueue&); //Functie de afisare a unei cozi
+            CharQueue& operator+(CharQueue myQueue); //Metoda a concatenare a doua cozi
+            CharQueue& operator-(CharQueue myQueue); //Metoda de diferentiere a doua cozi
+        };
+    }
 }
 
 #endif // POO_TEMA1_CHARQUEUE_H_
